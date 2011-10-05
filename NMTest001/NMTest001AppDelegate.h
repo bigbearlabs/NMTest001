951d53bf0b7e95753375da4ad41ed8bf6f45eab1
Nick Moore <nick@pilotmoon.com>
2011-10-05 13:31:30 +0100

    Initial Commit

---
+++ NMTest001/NMTest001AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NMTest001AppDelegate.h
+//  NMTest001
+//
+//  Created by Nick Moore on 05/10/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NMTest001AppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ NMTest001/NMTest001AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  NMTest001AppDelegate.m
+//  NMTest001
+//
+//  Created by Nick Moore on 05/10/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NMTest001AppDelegate.h"
+
+@implementation NMTest001AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
